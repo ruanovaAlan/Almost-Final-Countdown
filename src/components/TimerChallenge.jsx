2dac88068310be7ba80d5f1de26f42a44acfae0a
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-15 16:20:05 -0600

    Add useRef hook for timer and implement stop functionality

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
 
+    const timer = useRef(null);
+
     function handleStart() {
         setTimerStarted(true);
 
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime * 1000);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
     }
 
     return (
@@ -20,7 +26,7 @@ export default function TimerChallenge({ title, targetTime }) {
                 {targetTime} second{targetTime > 1 ? "s" : ""}
             </p>
             <p>
-                <button onClick={handleStart}>
+                <button onClick={timerStarted ? handleStop : handleStart}>
                     {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
