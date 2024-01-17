11e501b7c62599ddf354a4b46987b1b0e7961b54
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-16 21:26:07 -0600

    Refactor ResultModal and TimerChallenge components

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ onReset, targetTime, remainingTime }, ref) {
 
     const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     useImperativeHandle(ref, () => {
         return {
@@ -14,10 +17,10 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
 
     return (
         <dialog ref={dialog} className="result-modal" >
-            <h2>You {result}</h2>
-            <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog">
+            {userLost && <h2>You lost</h2>}
+            <p>The target time was <strong>{targetTime} seconds.</strong></p>
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
