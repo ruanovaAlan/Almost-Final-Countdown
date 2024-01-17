bc131b05311e869610283cbc977ef64f5ca57875
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-16 21:31:43 -0600

    Add score display in ResultModal component

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -6,6 +6,7 @@ const ResultModal = forwardRef(function ResultModal({ onReset, targetTime, remai
 
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -18,6 +19,7 @@ const ResultModal = forwardRef(function ResultModal({ onReset, targetTime, remai
     return (
         <dialog ref={dialog} className="result-modal" >
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
             <form method="dialog" onSubmit={onReset}>
